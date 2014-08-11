1a971882e47644057aa4fcc1cf17505dae52b679
Rick van Rein <rick@openfortress.nl>
2014-08-11 17:21:04 +0200

    Implemented according to Programming Assignment 2 of Coursera rprog-006

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Computing the inverse of a matrix can be costly, and caching it can be
+## a worthwhile facility.  The following functions implement just that.
+##
+## Note that it is being assumed that the matrix will be invertible.
 
-## Write a short comment describing this function
+## Construct an object that holds the matrix and its precalculated inverse.
+## The inverse may be NULL to indicate that it has not been created.
+##  - setMatrix(m) sets the matrix to m (and clears the inverse)
+##  - getMatrix() loads the matrix
+##  - setInverse(i) sets the matrix inverse to m
+##  - getInverse() loads the matrix inverse; the returned value may be NULL
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mat = matrix()) {
+        imat <- NULL
+        setMatrix <- function (m) {
+                mat <<- m
+                imat <<- NULL
+        }
+        getMatrix <- function () {
+                mat
+        }
+        setInverse <- function (i) {
+                imat <<- i
+        }
+        getInverse <- function () {
+                imat
+        }
 }
 
 
-## Write a short comment describing this function
+## The following function wraps around solve() and works on inverse-cached matrices
+## such as constructed by makeCacheMatrix() above.  When the inverse is already
+## present in the cached object, it will be loaded instead of recalculated.
+##
+## Note: The reliable functioning of this operation revolves around the use of
+##       setMatrix() to set the matrix in all other code, rather then accessing the
+##       variables directly; there is no such thing as encapsulation.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+        i <- m$getInverse ()
+        if (is.null (i)) {
+                message "Recalculating inverse matrix"
+                i <- solve (m$getMatrix (), ...)
+                m$setInverse (i)
+        }
+        i
 }
